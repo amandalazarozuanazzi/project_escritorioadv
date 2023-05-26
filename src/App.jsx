75e2e89b975a37885dee063fb39a956609320594
amanda.lazarozuanazzi <amanda.lazarozuanazzi@gmail.com>
2023-05-26 18:53:42 +0100

    primeira versão

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+    <header>
+      <h1>Flório & Cândido Advogados</h1>
+    </header>
+    <div>
+      <main>
+        <p>app Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia asperiores molestiae dolore, eligendi nemo soluta. Consequuntur omnis ut cupiditate autem nam laborum ex soluta praesentium pariatur reprehenderit laboriosam, dignissimos dicta veniam. Distinctio eligendi eius nam culpa consequatur voluptatum corrupti nisi. Sunt fugiat atque distinctio doloremque! Atque, error. Optio dicta, doloremque cumque ipsum, tenetur fugiat ut sequi inventore dignissimos laudantium molestias, vel eaque doloribus consequatur! Odio at animi fuga, dolorum perspiciatis voluptatibus atque dolor impedit architecto in. Dolorum corporis perferendis voluptates alias labore? Eaque eum possimus, iusto mollitia omnis hic ducimus sit earum voluptas officiis, inventore in, temporibus ipsam facilis dignissimos?</p>
+      </main>
+      <footer>Dra. Camyla de Oliveira Flório Cândido e Dr. Michael Cândido<br>
+      </br>Flório & Cândido Advogados - Sociedade de Advogados, Vinhedo - SP. Todos os direitos reservados.</footer>
+    </div>
+    </>
+  )
+}
+
+export default App
